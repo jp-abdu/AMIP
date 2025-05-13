a433ff4225f9b9554c64a3c709e71f958912c5bd
Enzo Perroni <142441388+Enzoperroni@users.noreply.github.com>
2025-05-13 16:47:14 -0300

    arrumando a pagina home view

--- TesteAmip/Views/HomeView.swift
+++ TesteAmip/Views/HomeView.swift
@@ -15,6 +15,7 @@ struct HomeView: View {
                             .scaledToFit()
                             .frame(width: 180, height: 180)
                             .padding(.top, 10)
+                            .offset(y:-90)
                     }
                     .frame(maxWidth: .infinity, alignment: .top) // Garantindo que a logo fique no topo
 
@@ -25,30 +26,36 @@ struct HomeView: View {
                         .font(.title2)
                         .bold()
                         .multilineTextAlignment(.center)
+                        .offset(y:-110)
 
                     Text("Abaixo você pode encontrar o questionário:")
                         .font(.subheadline)
                         .foregroundColor(.gray)
+                        .offset(y:-115)
+                        .padding(.bottom, 20)
 
                     // Caixa centralizada com borda
                     VStack {
                         Text("Recenseamento Ilha Primeira")
                             .font(.headline)
                             .padding(.top, 10)
+                            .offset(y:-70)
 
                         Image("census_image")
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 200, height: 200)
+                            .frame(width: 200, height: 270)
                             .padding(.vertical, 10)
-
+                            .offset(y:-70)
                         HStack {
                             VStack(alignment: .leading) {
                                 Text("Recenseamento")
                                     .font(.subheadline)
+                                    .offset(y:-70)
                                 Text("Menos de 15 minutos")
                                     .font(.caption)
                                     .foregroundColor(.gray)
+                                    .offset(y:-70)
                             }
 
                             Spacer()
@@ -60,17 +67,20 @@ struct HomeView: View {
                                     .background(Color(red: 0.68, green: 0.89, blue: 0.82))
                                     .foregroundColor(.black)
                                     .cornerRadius(15)
+                                    .offset(y:-70)
                             }
                         }
                         .padding(.horizontal, 20)
                         .padding(.bottom, 10)
+                        
                     }
                     .overlay(
                         RoundedRectangle(cornerRadius: 20)
                             .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                            .offset(y:-70)
                     )
                     .padding(.horizontal, 20)
-
+                    .offset(y:-50)
                     Spacer()
                 }
             }
